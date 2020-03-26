d9cad20d09f70960d6a42d2716d52998df6a016b
tamimcse <tamim@csebuet.org>
2020-03-25 20:03:27 -0400

    .

---
+++ setpaths.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# This is a bash utility script to set the paths to the current
+# directory of OpenRAM. It must be sourced in the local directory
+# like this:
+# source setpaths.sh
+
+export OPENRAM_HOME="/home/tamim/OpenRAM/compiler"
+export OPENRAM_TECH="/home/tamim/OpenRAM/technology"
+export FREEPDK45="/home/tamim/Downloads/FreePDK45"
