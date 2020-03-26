f44d6a9b60900d9fea84310dad8e5320eb611393
tamimcse <tamim@csebuet.org>
2020-03-25 20:41:40 -0400

    .

--- run.sh
+++ run.sh
@@ -1,2 +1,5 @@
-python3 $OPENRAM_HOME/openram.py  sail/N16
-python3 $OPENRAM_HOME/openram.py  sail/C16
+#python3 $OPENRAM_HOME/openram.py  sail/N16
+#python3 $OPENRAM_HOME/openram.py  sail/C16
+python3 $OPENRAM_HOME/openram.py  sail/N24
+python3 $OPENRAM_HOME/openram.py  sail/C24
+
